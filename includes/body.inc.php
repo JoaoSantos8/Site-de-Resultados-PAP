9593a4f9c9ef1e12a3e8826152afd504e55367be
Leandro Pereira <34716429+mandmcoders@users.noreply.github.com>
2022-03-24 11:39:24 +0000

    Classificações

---
+++ includes/body.inc.php
@@ -0,0 +1,14 @@
+<?php
+include_once ("admin/includes/config.inc.php");
+$con=mysqli_connect(HOST,USER,PASSWORD,DATABASE);
+
+function top(){
+    ?>
+
+<?php
+}
+function bottom(){
+    ?>
+
+<?php
+}
