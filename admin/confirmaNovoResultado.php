60e8592e4c290f68d54b1fcd6e05a8a0e36fb9f1
Leandro Pereira <34716429+mandmcoders@users.noreply.github.com>
2022-07-12 11:21:53 +0100

    NovoResultado

---
+++ admin/confirmaNovoResultado.php
@@ -0,0 +1,38 @@
+<?php
+include_once ("includes/body.inc.php");
+global $con;
+
+$jogoId=intval($_POST['jogoId']);
+$jogoId=intval($_POST['jogoId']);
+$jogoId=intval($_POST['jogoId']);
+$jogoId=intval($_POST['jogoId']);
+$jogoId=intval($_POST['jogoId']);
+
+
+/*
+ //$sql="update resultados
+set resultadoGolosCasa=10
+, resultadoGolosFora=20"
+
+mysqli_query($con,$sql);
+
+update pontos
+set pontosResultado='V'
+, pontosValor=3
+where pontosJogoId=1 and pontosClubeId=1;
+mysqli_query($con,$sql);
+
+
+
+update pontos
+set pontosResultado='D'
+, pontosValor=0
+where pontosJogoId=1 and pontosClubeId=10;
+
+mysqli_query($con,$sql);
+ */
+
+
+
+//header("location:listaClubes.php");
+?>
