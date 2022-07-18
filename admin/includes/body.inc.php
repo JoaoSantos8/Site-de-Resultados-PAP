e056461b61c25b5ee33c382f08e89df46a3241dc
JoaoSantos8 <joaosantos151442@gmail.com>
2022-07-18 16:06:43 +0100

    imagens

--- admin/includes/body.inc.php
+++ admin/includes/body.inc.php
@@ -41,7 +41,6 @@ function drawTop($menu=HOME,$page=''){
     <html lang="en">
     <head>
         <!-- Required meta tags -->
-        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <!-- Bootstrap CSS -->
